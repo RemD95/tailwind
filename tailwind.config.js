06219a98f3a8a11578dd1a39db529a43fdf2b3aa
Remus Daniel Blaj <remoblaj@gmail.com>
2025-06-25 15:33:36 +0200

    tema personalizzato

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,15 @@ export default {
       colors: {
         primary: '#2757a9',
         btnbg: '#9927a9',
+        btntxt:'#c90076'
       },
       spacing: {
         '1': '0.25rem',
         '2': '0.5rem',
       },
+      fontFamily: {
+        'xxl': '2rem'
+      }
     },
   },
   plugins: [],
